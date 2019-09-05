e2a3ff77c146b9338958044465facc52ef7797ee
Walter White <WalterWhite@walterjwhite.com>
2019-09-04 21:47:53 -0400

    merged

--- api/src/main/java/com/walterjwhite/web/service/ProtocolVersion.java
+++ api/src/main/java/com/walterjwhite/web/service/ProtocolVersion.java
@@ -1,8 +1,10 @@
 package com.walterjwhite.web.service;
 
 import lombok.Data;
+import lombok.ToString;
 
 @Data
+@ToString(doNotUseGetters = true)
 public class ProtocolVersion {
   protected String majorVersion;
   protected String minorVersion;
